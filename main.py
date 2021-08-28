965c52238db6035e99523c194872bcb7ee31fc22
Soare95 <soaremihai.a@yahoo.com>
2021-08-28 15:54:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+website = response.text
+soup = BeautifulSoup(website, "html.parser")
+
+content = soup.find_all(class_="jsx-4245974604")
+
+
+
+
+
